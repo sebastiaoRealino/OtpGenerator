8bd060c5c405c867a580bba7fc620b8360077dfa
Sebastião Edélcio Oliveira Realino <sebastiaorealino@inatel.br>
2018-07-10 18:21:48 -0300

    feat(base): base project created

---
+++ app/src/main/cpp/otpjni.cpp
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <jni.h>
+#include <stdlib.h>
+#include "otp.h"
+
+#define LOGD(...) \
+  ((void)__android_log_print(ANDROID_LOG_DEBUG, "EncryptionLib-JNI", __VA_ARGS__))
+
+
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_com_example_sebastiaorealino_otpgen_MainActivity_generateOtp(JNIEnv *env, jobject thiz, jstring key) {
+
+    const char *constKeyLocal = env->GetStringUTFChars(key, 0);
+    char* keyLocal = (char*)constKeyLocal;
+
+    int keysize = 32;
+
+    char* digest= (char*)malloc(20);
+    memset(digest, 0, 20);
+
+    generateotp(keyLocal,keysize,digest);
+
+    jbyte*digestLocal = (jbyte*)calloc(sizeof(jbyte), 20);
+    for(int i=0; i <= 20; i++){
+        digestLocal[i] =  (jbyte) digest[i];
+    }
+
+    jbyteArray digestByteArray = env->NewByteArray(20);
+    env->SetByteArrayRegion(digestByteArray, 0, 20 , digestLocal);
+
+    free(digestLocal);
+    free(digest);
+    free(keyLocal);
+
+    return digestByteArray;
+}
