b20e0bb2fb153994b238bd177c812168bf4677c5
Sebastião Edélcio Oliveira Realino <sebastiaorealino@inatel.br>
2018-07-10 22:56:15 -0300

    feat(otp): OTP genereted without QRCode reader

--- app/src/main/java/com/example/sebastiaorealino/otpgen/MainActivity.java
+++ app/src/main/java/com/example/sebastiaorealino/otpgen/MainActivity.java
@@ -4,15 +4,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Formatter;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,30 +34,71 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                generateOTP();
             }
         });
 
+        generateOTP();
+
+    }
+    public void generateOTP(){
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
 
         byte[] otpByte = generateOtp("QYDVQQLEwpUZWNoIERlcHQuMSgwJgYDV");
+        byte[] byteRange = Arrays.copyOfRange(otpByte, 10, 14);
 
-        try {
-            String toStr = new String(otpByte, "ISO-8859-1");
-            Log.i("OTP", toHexString(otpByte));
-            tv.setText(toHexString(otpByte));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+        String hexValue = truncateWhenUTF8("0x"+toHexString(byteRange),31);
+
+        Log.d("OTP HEx", hexValue);
+
+        int otp = getDecimal(hexValue);
+        String fullOtp = String.valueOf(otp);
+        String sOtpGenereted = fullOtp.substring(fullOtp.length() -6, fullOtp.length());
+
+        Log.d("OTP OTP", sOtpGenereted);
+        tv.setText(sOtpGenereted);
+    }
+
+    public static int getDecimal(String hex){
+        String digits = "0123456789ABCDEF";
+        hex = hex.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < hex.length(); i++)
+        {
+            char c = hex.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
         }
+        return val;
+    }
 
-
-//        for (byte item: otpByte) {
-//            String str = new String(otpByte, StandardCharsets.UTF_8);
-//            Log.d(item.toString());
-//        }
-
+    public static String truncateWhenUTF8(String s, int maxBytes) {
+        int b = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int skip = 0;
+            int more;
+            if (c <= 0x007f) {
+                more = 1;
+            }
+            else if (c <= 0x07FF) {
+                more = 2;
+            } else if (c <= 0xd7ff) {
+                more = 3;
+            } else if (c <= 0xDFFF) {
+                more = 4;
+                skip = 1;
+            } else {
+                more = 3;
+            }
+            if (b + more > maxBytes) {
+                return s.substring(0, i);
+            }
+            b += more;
+            i += skip;
+        }
+        return s;
     }
 
     private static String toHexString(byte[] bytes) {
@@ -68,7 +110,16 @@ public class MainActivity extends AppCompatActivity {
 
         return formatter.toString();
     }
+    public static int truncateUtf8(String input, byte[] output) {
 
+        ByteBuffer outBuf = ByteBuffer.wrap(output);
+        CharBuffer inBuf = CharBuffer.wrap(input.toCharArray());
+
+        Charset utf8 = Charset.forName("UTF-8");
+        utf8.newEncoder().encode(inBuf, outBuf, true);
+        System.out.println("encoded " + inBuf.position() + " chars of " + input.length() + ", result: " + outBuf.position() + " bytes");
+        return outBuf.position();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
